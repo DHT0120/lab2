e3612ead7e4531e97fa787efb26a9763dc4c23fe
dht <dht20040120@outlook.com>
2024-10-25 15:10:38 +0800

    4.6
    4.6

--- 331-intro-to-vite/src/App.vue
+++ 331-intro-to-vite/src/App.vue
@@ -1,10 +1,10 @@
 <script setup lang="ts">
 import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
+
 </script>
 
 <template>
-  <header>
+  <!-- <header>
     <img
       alt="Vue logo"
       class="logo"
@@ -14,8 +14,7 @@ import HelloWorld from './components/HelloWorld.vue'
     />
 
     <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
+      
       <nav>
         <RouterLink to="/">Home</RouterLink>
         <RouterLink to="/about">About</RouterLink>
@@ -23,69 +22,93 @@ import HelloWorld from './components/HelloWorld.vue'
     </div>
   </header>
 
-  <RouterView />
+  <RouterView /> -->
+  <div id="layout">
+    <header>
+      <div class="wrapper">
+        <nav>
+          <RouterLink to="/">Home</RouterLink> |
+          <RouterLink to="/about">About</RouterLink>
+        </nav>
+      </div>
+    </header>
+
+    <RouterView />
+  </div>
 </template>
 
 <style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
-}
+#layout {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-nav {
-  width: 100%;
-  font-size: 12px;
+/* header { */
+  /* line-height: 1.5; */
+  /* max-height: 100vh; */
+/* } */
+/*  */
+/* .logo { */
+  /* display: block; */
+  /* margin: 0 auto 2rem; */
+/* } */
+/*  */
+/* nav { */
+  /* width: 100%; */
+  /* font-size: 12px; */
   text-align: center;
   margin-top: 2rem;
+  color: #2c3e50;
 }
 
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
+/* nav a.router-link-exact-active { */
+  /* color: var(--color-text); */
+/* } */
+/*  */
+/* nav a.router-link-exact-active:hover { */
+  /* background-color: transparent; */
+  nav{
+    padding: 30px;
 }
 
 nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
+  font-weight: bold;
+  color: #2c3e50;
+  /* display: inline-block; */
+  /* padding: 0 1rem; */
+  /* border-left: 1px solid var(--color-border); */
 }
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
+/*  */
+/* nav a:first-of-type { */
+  /* border: 0; */
+/* } */
+/*  */
+/* @media (min-width: 1024px) { */
+  /* header { */
+    /* display: flex; */
+    /* place-items: center; */
+    /* padding-right: calc(var(--section-gap) / 2); */
+  /* } */
+/*  */
+  /* .logo { */
+    /* margin: 0 2rem 0 0; */
+  /* } */
+/*  */
+  /* header .wrapper { */
+    /* display: flex; */
+    /* place-items: flex-start; */
+    /* flex-wrap: wrap; */
+  /* } */
+/*  */
+  /* nav { */
+    /* text-align: left; */
+    /* margin-left: -1rem; */
+    /* font-size: 1rem; */
+/*  */
+    /* padding: 1rem 0; */
+    /* margin-top: 1rem; */
+  /* } */
+  nav a.router-link-exact-active {
+    color: #42b983;
 }
 </style>
--- 331-intro-to-vite/src/views/HomeView.vue
+++ 331-intro-to-vite/src/views/HomeView.vue
@@ -1,9 +1,10 @@
-<script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
-</script>
+<!-- <script setup lang="ts"> -->
+// import TheWelcome from '../components/TheWelcome.vue'
+<!-- </script> -->
 
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+  <!-- <main> -->
+    <!-- <TheWelcome /> -->
+  <!-- </main> -->
+  <div class="home"></div>
 </template>
