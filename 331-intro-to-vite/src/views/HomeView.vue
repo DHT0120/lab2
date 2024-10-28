99828c54b6e7261574ffe1c1fd19176a1f00894c
dht <dht20040120@outlook.com>
2024-10-28 10:53:02 +0800

    8.7
    8.7

--- 331-intro-to-vite/src/views/HomeView.vue
+++ 331-intro-to-vite/src/views/HomeView.vue
@@ -1,10 +1,8 @@
-<!-- <script setup lang="ts"> -->
-// import TheWelcome from '../components/TheWelcome.vue'
-<!-- </script> -->
 <script setup lang="ts">
-import EventCard from '@/components/EventCard.vue'
-import type { Event } from '@/types'
-import { ref } from 'vue'
+import EventCard from '@/components/EventCard.vue';
+import EventCategoriesAndOrganizer from '@/components/EventCategoriesAndOrganizer.vue';
+import type { Event } from '@/types.ts'
+import { ref } from 'vue';
 const events = ref<Event[]>([
   {
     id: 5928101,
@@ -43,10 +41,50 @@ const events = ref<Event[]>([
 </script>
 
 <template>
-  <!-- <main> -->
-    <!-- <TheWelcome /> -->
-  <!-- </main> -->
-  <div class="home">
+  <div class="events">
     <EventCard v-for="event in events" :key="event.id" :event="event" />
+    <div class="event-card">
+      <EventCategoriesAndOrganizer
+      :organizer-name="event.organizer"
+      :categories="[event.category]"
+      v-for="event in events"
+      :key="`${event.id}-info`"
+    /></div>
   </div>
 </template>
+
+
+<style scoped>
+.events {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+.event-categories-and-organizer {
+  text-align: right;
+  width: 100%;
+}
+.event-card {
+  padding: 20px;
+  width: 250px;
+  cursor: pointer;
+  border: 1px solid #39495c;
+  margin-bottom: 18px;
+  box-sizing: border-box; /* 确保边框和内边距包含在总宽度和高度内 */
+}
+.event-card:hover {
+  transform: scale(1.01);
+  box-shadow: 0 3px 12px 0 rgba(0, 0, 0, 0.2);
+}
+
+.event-categories-and-organizer {
+  display: flex;
+  justify-content: flex-end;
+  font-size: 16px; /* 调整字体大小以匹配需求 */
+  margin-top: 10px; /* 与事件卡片之间的间距 */
+}
+
+.text-align-right {
+  margin-left: 10px; /* 可以调整间距 */
+}
+</style>
